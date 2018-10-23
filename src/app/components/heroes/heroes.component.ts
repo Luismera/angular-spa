b6f90cfa1ea99473ee2d307981127f09b5faae8a
Carlos Mercado <carlos.mercado@3creatives.com.co>
2018-10-22 19:06:47 -0500

    detalle del heroe

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService } from '../../services/heroes.service';
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-heroes',
@@ -10,11 +10,18 @@ export class HeroesComponent implements OnInit {
   
   heroes: any[] = [];
 
-  constructor(private _heroesService: HeroesService) { }
+  constructor(private _heroesService: HeroesService,
+              private router:Router
+              ) { }
 
   ngOnInit() {
     this.heroes = this._heroesService.getHeroes();
     console.log(this.heroes)
+  }
+
+  verHeroe( idx:number ){
+    console.log(idx)
+    this.router.navigate( ['/heroe', idx] )
   }
 
 }
